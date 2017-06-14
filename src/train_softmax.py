df67a5d771bf7bba0b4e9d753454d36a2a5cd8f5
luuuyi <435977170@qq.com>
2017-06-14 11:44:47 +0800

    luyi at DSP upload in 2017/06/14, translate train_softmax.py file

--- src/train_softmax.py
+++ src/train_softmax.py
@@ -45,9 +45,11 @@ from tensorflow.python.framework import ops
 from tensorflow.python.ops import array_ops
 
 def main(args):
-  
+    # 主函数入口
+    # 从model模块中选择要使用的训练结构
     network = importlib.import_module(args.model_def)
 
+    # 日志路径和模型保存路径的子目录命名结构
     subdir = datetime.strftime(datetime.now(), '%Y%m%d-%H%M%S')
     log_dir = os.path.join(os.path.expanduser(args.logs_base_dir), subdir)
     if not os.path.isdir(log_dir):  # Create the log directory if it doesn't exist
@@ -57,17 +59,22 @@ def main(args):
         os.makedirs(model_dir)
 
     # Store some git revision info in a text file in the log directory
+    # 当前脚本的的绝对路径，存储相关信息到log文件中
     src_path,_ = os.path.split(os.path.realpath(__file__))
     facenet.store_revision_info(src_path, log_dir, ' '.join(sys.argv))
 
+    # 用参数初始化numpy随机器
     np.random.seed(seed=args.seed)
     random.seed(args.seed)
+    # 获取训练数据
     train_set = facenet.get_dataset(args.data_dir)
     if args.filter_filename:
         train_set = filter_dataset(train_set, os.path.expanduser(args.filter_filename), 
             args.filter_percentile, args.filter_min_nrof_images_per_class)
+    # 训练数据子类别数目
     nrof_classes = len(train_set)
     
+    # 打印模型和log保存路径，如果有预训练模型需要加载即加载
     print('Model directory: %s' % model_dir)
     print('Log directory: %s' % log_dir)
     pretrained_model = None
@@ -75,6 +82,7 @@ def main(args):
         pretrained_model = os.path.expanduser(args.pretrained_model)
         print('Pre-trained model: %s' % pretrained_model)
     
+    # 如果指定了lfw验证数据路径即打印，并且加载lfw数据配对文件及相关信息
     if args.lfw_dir:
         print('LFW directory: %s' % args.lfw_dir)
         # Read the file containing the pairs used for testing
@@ -82,20 +90,26 @@ def main(args):
         # Get the paths for the corresponding images
         lfw_paths, actual_issame = lfw.get_paths(os.path.expanduser(args.lfw_dir), pairs, args.lfw_file_ext)
     
+    # 新建一个图，将该图作为默认图，进入上下文管理器
     with tf.Graph().as_default():
+        # 设置随机种子和全局
         tf.set_random_seed(args.seed)
         global_step = tf.Variable(0, trainable=False)
         
         # Get a list of image paths and their labels
+        # 得到训练数据的列表和相对应的标签列表
         image_list, label_list = facenet.get_image_paths_and_labels(train_set)
         assert len(image_list)>0, 'The dataset should not be empty'
         
         # Create a queue that produces indices into the image_list and label_list 
+        # 将标签列表转化为tensor形式的列表，并获取tensor的范围
         labels = ops.convert_to_tensor(label_list, dtype=tf.int32)
         range_size = array_ops.shape(labels)[0]
+        # 
         index_queue = tf.train.range_input_producer(range_size, num_epochs=None,
                              shuffle=True, seed=None, capacity=32)
         
+        # 构建训练标签队列，并设置训练中的参数
         index_dequeue_op = index_queue.dequeue_many(args.batch_size*args.epoch_size, 'index_dequeue')
         
         learning_rate_placeholder = tf.placeholder(tf.float32, name='learning_rate')
@@ -108,22 +122,27 @@ def main(args):
 
         labels_placeholder = tf.placeholder(tf.int64, shape=(None,1), name='labels')
         
+        # 构建训练数据的队列，训练数据，训练标签配对
         input_queue = data_flow_ops.FIFOQueue(capacity=100000,
                                     dtypes=[tf.string, tf.int64],
                                     shapes=[(1,), (1,)],
                                     shared_name=None, name=None)
         enqueue_op = input_queue.enqueue_many([image_paths_placeholder, labels_placeholder], name='enqueue_op')
         
+        # 数据预处理线程数
         nrof_preprocess_threads = 4
         images_and_labels = []
+        # 使用线程去并行处理数据
         for _ in range(nrof_preprocess_threads):
             filenames, label = input_queue.dequeue()
             images = []
+            # 读取队列中的配对数据，读取训练图像和训练标签，并对数据进行预处理
             for filename in tf.unstack(filenames):
                 file_contents = tf.read_file(filename)
                 image = tf.image.decode_png(file_contents)
                 if args.random_rotate:
                     image = tf.py_func(facenet.random_rotate_image, [image], tf.uint8)
+                # 如果没开启随机截取的开关，即选择图像正中的位置进行训练
                 if args.random_crop:
                     image = tf.random_crop(image, [args.image_size, args.image_size, 3])
                 else:
@@ -132,10 +151,12 @@ def main(args):
                     image = tf.image.random_flip_left_right(image)
     
                 #pylint: disable=no-member
+                # 再一次确认图像分辨率
                 image.set_shape((args.image_size, args.image_size, 3))
                 images.append(tf.image.per_image_standardization(image))
             images_and_labels.append([images, label])
-    
+        
+        # 配置训练batch的参数
         image_batch, label_batch = tf.train.batch_join(
             images_and_labels, batch_size=batch_size_placeholder, 
             shapes=[(args.image_size, args.image_size, 3), ()], enqueue_many=True,
@@ -145,25 +166,31 @@ def main(args):
         image_batch = tf.identity(image_batch, 'input')
         label_batch = tf.identity(label_batch, 'label_batch')
         
+        # 打印最终处理完的数据信息
         print('Total number of classes: %d' % nrof_classes)
         print('Total number of examples: %d' % len(image_list))
         
         print('Building training graph')
         
         # Build the inference graph
+        # 构建TensorFlow数据流图
         prelogits, _ = network.inference(image_batch, args.keep_probability, 
             phase_train=phase_train_placeholder, bottleneck_layer_size=args.embedding_size, 
             weight_decay=args.weight_decay)
+        # 连接全连接层
         logits = slim.fully_connected(prelogits, len(train_set), activation_fn=None, 
                 weights_initializer=tf.truncated_normal_initializer(stddev=0.1), 
                 weights_regularizer=slim.l2_regularizer(args.weight_decay),
                 scope='Logits', reuse=False)
-
+        
+        # 对特征维度（128维）做一个L2正则化处理
         embeddings = tf.nn.l2_normalize(prelogits, 1, 1e-10, name='embeddings')
 
         # Add center loss
+        # 添加center loss
         if args.center_loss_factor>0.0:
             prelogits_center_loss, _ = facenet.center_loss(prelogits, label_batch, args.center_loss_alfa, nrof_classes)
+            #
             tf.add_to_collection(tf.GraphKeys.REGULARIZATION_LOSSES, prelogits_center_loss * args.center_loss_factor)
 
         learning_rate = tf.train.exponential_decay(learning_rate_placeholder, global_step,
@@ -171,26 +198,32 @@ def main(args):
         tf.summary.scalar('learning_rate', learning_rate)
 
         # Calculate the average cross entropy loss across the batch
+        # 计算交叉熵
         cross_entropy = tf.nn.sparse_softmax_cross_entropy_with_logits(
             labels=label_batch, logits=logits, name='cross_entropy_per_example')
         cross_entropy_mean = tf.reduce_mean(cross_entropy, name='cross_entropy')
         tf.add_to_collection('losses', cross_entropy_mean)
         
         # Calculate the total losses
+        # 计算总的loss
         regularization_losses = tf.get_collection(tf.GraphKeys.REGULARIZATION_LOSSES)
         total_loss = tf.add_n([cross_entropy_mean] + regularization_losses, name='total_loss')
 
         # Build a Graph that trains the model with one batch of examples and updates the model parameters
+        # 构建总的训练方式
         train_op = facenet.train(total_loss, global_step, args.optimizer, 
             learning_rate, args.moving_average_decay, tf.global_variables(), args.log_histograms)
         
         # Create a saver
+        # 构建一个保存对象
         saver = tf.train.Saver(tf.trainable_variables(), max_to_keep=3)
 
         # Build the summary operation based on the TF collection of Summaries.
+        # 构建一个总的训练摘要对象
         summary_op = tf.summary.merge_all()
 
         # Start running operations on the Graph.
+        # 开始训练，配置GPU使用参数
         gpu_options = tf.GPUOptions(per_process_gpu_memory_fraction=args.gpu_memory_fraction)
         sess = tf.Session(config=tf.ConfigProto(gpu_options=gpu_options, log_device_placement=False))
         sess.run(tf.global_variables_initializer())
@@ -206,6 +239,7 @@ def main(args):
                 saver.restore(sess, pretrained_model)
 
             # Training and validation loop
+            # 开始训练循环
             print('Running training')
             epoch = 0
             while epoch < args.max_nrof_epochs:
@@ -256,7 +290,8 @@ def filter_dataset(dataset, data_filename, percentile, min_nrof_images_per_class
             del(filtered_dataset[i])
 
     return filtered_dataset
-  
+
+# 训练子函数
 def train(args, sess, epoch, image_list, label_list, index_dequeue_op, enqueue_op, image_paths_placeholder, labels_placeholder, 
       learning_rate_placeholder, phase_train_placeholder, batch_size_placeholder, global_step, 
       loss, train_op, summary_op, summary_writer, regularization_losses, learning_rate_schedule_file):
@@ -267,6 +302,7 @@ def train(args, sess, epoch, image_list, label_list, index_dequeue_op, enqueue_o
     else:
         lr = facenet.get_learning_rate_from_file(learning_rate_schedule_file, epoch)
 
+    # 获取当前batch要训练的数据
     index_epoch = sess.run(index_dequeue_op)
     label_epoch = np.array(label_list)[index_epoch]
     image_epoch = np.array(image_list)[index_epoch]
@@ -298,6 +334,7 @@ def train(args, sess, epoch, image_list, label_list, index_dequeue_op, enqueue_o
     summary_writer.add_summary(summary, step)
     return step
 
+# 在lfw数据集上验证子函数
 def evaluate(sess, enqueue_op, image_paths_placeholder, labels_placeholder, phase_train_placeholder, batch_size_placeholder, 
         embeddings, labels, image_paths, actual_issame, batch_size, nrof_folds, log_dir, step, summary_writer):
     start_time = time.time()
@@ -337,6 +374,7 @@ def evaluate(sess, enqueue_op, image_paths_placeholder, labels_placeholder, phas
     with open(os.path.join(log_dir,'lfw_result.txt'),'at') as f:
         f.write('%d\t%.5f\t%.5f\n' % (step, np.mean(accuracy), val))
 
+# 保存训练摘要子函数，meta-graph只保存一次，variables每一次都保存
 def save_variables_and_metagraph(sess, saver, summary_writer, model_dir, model_name, step):
     # Save the model checkpoint
     print('Saving variables')
@@ -361,87 +399,125 @@ def save_variables_and_metagraph(sess, saver, summary_writer, model_dir, model_n
   
 
 def parse_arguments(argv):
+    #参数解析部分
     parser = argparse.ArgumentParser()
     
+    #存放log文件的路径
     parser.add_argument('--logs_base_dir', type=str, 
         help='Directory where to write event logs.', default='~/logs/facenet')
+    #存放模型文件及权值文件的路径（checkpoint，meta，data）
     parser.add_argument('--models_base_dir', type=str,
         help='Directory where to write trained models and checkpoints.', default='~/models/facenet')
+    #每一块GPU最多使用的显存比例
     parser.add_argument('--gpu_memory_fraction', type=float,
         help='Upper bound on the amount of GPU memory that will be used by the process.', default=1.0)
+    #预训练模型的路径
     parser.add_argument('--pretrained_model', type=str,
         help='Load a pretrained model before training starts.')
+    #训练数据的路径（已经对齐后的人脸数据），需要根据身份以子目录的形式区分
     parser.add_argument('--data_dir', type=str,
         help='Path to the data directory containing aligned face patches. Multiple directories are separated with colon.',
         default='~/datasets/casia/casia_maxpy_mtcnnalign_182_160')
+    #使用模型的定义，默认是作者使用的inception-resnet
     parser.add_argument('--model_def', type=str,
         help='Model definition. Points to a module containing the definition of the inference graph.', default='models.inception_resnet_v1')
+    #最大迭代次数
     parser.add_argument('--max_nrof_epochs', type=int,
         help='Number of epochs to run.', default=500)
+    #每一个batch需要训练图片张数
     parser.add_argument('--batch_size', type=int,
         help='Number of images to process in a batch.', default=90)
+    #训练图像的分辨率，160x160
     parser.add_argument('--image_size', type=int,
         help='Image size (height, width) in pixels.', default=160)
+    #一次训练迭代的batch数
     parser.add_argument('--epoch_size', type=int,
         help='Number of batches per epoch.', default=1000)
+    #人脸提取特征的维度，默认128
     parser.add_argument('--embedding_size', type=int,
         help='Dimensionality of the embedding.', default=128)
+    #训练时160的分辨率样本从182的分辨率上随机截取
     parser.add_argument('--random_crop', 
         help='Performs random cropping of training images. If false, the center image_size pixels from the training images are used. ' +
          'If the size of the images in the data directory is equal to image_size no cropping is performed', action='store_true')
+    #采样随机水平抖动？
     parser.add_argument('--random_flip', 
         help='Performs random horizontal flipping of training images.', action='store_true')
+    #采样随机旋转
     parser.add_argument('--random_rotate', 
         help='Performs random rotations of training images.', action='store_true')
+    #使用dropout时的保持概率
     parser.add_argument('--keep_probability', type=float,
         help='Keep probability of dropout for the fully connected layer(s).', default=1.0)
+    #训练时的权重衰减系数
     parser.add_argument('--weight_decay', type=float,
         help='L2 weight regularization.', default=0.0)
+    #反卷积损失函数参数
     parser.add_argument('--decov_loss_factor', type=float,
         help='DeCov loss factor.', default=0.0)
+    #center loss函数参数
     parser.add_argument('--center_loss_factor', type=float,
         help='Center loss factor.', default=0.0)
+    #center loss函数参数α，更新参数
     parser.add_argument('--center_loss_alfa', type=float,
         help='Center update rate for center loss.', default=0.95)
+    #优化方法
     parser.add_argument('--optimizer', type=str, choices=['ADAGRAD', 'ADADELTA', 'ADAM', 'RMSPROP', 'MOM'],
         help='The optimization algorithm to use', default='ADAGRAD')
+    #学习率，如果是负数，则从文件里面读取
     parser.add_argument('--learning_rate', type=float,
         help='Initial learning rate. If set to a negative value a learning rate ' +
         'schedule can be specified in the file "learning_rate_schedule.txt"', default=0.1)
+    #学习率衰减的周期
     parser.add_argument('--learning_rate_decay_epochs', type=int,
         help='Number of epochs between learning rate decay.', default=100)
+    #学习率衰减系数
     parser.add_argument('--learning_rate_decay_factor', type=float,
         help='Learning rate decay factor.', default=1.0)
+    #找寻训练参数的指数衰减
     parser.add_argument('--moving_average_decay', type=float,
         help='Exponential decay for tracking of training parameters.', default=0.9999)
+    #随机器种子
     parser.add_argument('--seed', type=int,
         help='Random seed.', default=666)
+    #预处理线程数（用于数据载入和数据增广）
     parser.add_argument('--nrof_preprocess_threads', type=int,
         help='Number of preprocessing (data loading and augumentation) threads.', default=4)
+    #权重，偏移log开关
     parser.add_argument('--log_histograms', 
         help='Enables logging of weight/bias histograms in tensorboard.', action='store_true')
+    #学习率调整规则文件
     parser.add_argument('--learning_rate_schedule_file', type=str,
         help='File containing the learning rate schedule that is used when learning_rate is set to to -1.', default='data/learning_rate_schedule.txt')
+    #数据集过滤文件
     parser.add_argument('--filter_filename', type=str,
         help='File containing image data used for dataset filtering', default='')
+    #与过滤文件相关的系数
     parser.add_argument('--filter_percentile', type=float,
         help='Keep only the percentile images closed to its class center', default=100.0)
+    #只保留大于等于一定数量样本的训练对象
     parser.add_argument('--filter_min_nrof_images_per_class', type=int,
         help='Keep only the classes with this number of examples or more', default=0)
  
     # Parameters for validation on LFW
+    #lfw验证数据的信息匹配文件
     parser.add_argument('--lfw_pairs', type=str,
         help='The file containing the pairs to use for validation.', default='data/pairs.txt')
+    #lfw验证数据的文件后缀
     parser.add_argument('--lfw_file_ext', type=str,
         help='The file extension for the LFW dataset.', default='png', choices=['jpg', 'png'])
+    #lfw验证数据的路径
     parser.add_argument('--lfw_dir', type=str,
         help='Path to the data directory containing aligned face patches.', default='')
+    #lfw验证数据每个batch包含图像张数
     parser.add_argument('--lfw_batch_size', type=int,
         help='Number of images to process in a batch in the LFW test set.', default=100)
+    #lfw验证数据交叉验证的分割系数
     parser.add_argument('--lfw_nrof_folds', type=int,
         help='Number of folds to use for cross validation. Mainly used for testing.', default=10)
     return parser.parse_args(argv)
   
 
 if __name__ == '__main__':
+    # 函数主入口，并且解析参数传入
     main(parse_arguments(sys.argv[1:]))
